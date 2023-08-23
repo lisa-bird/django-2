e4fc340d42c69015a411bf06e56211e0bea2a879
lisa-bird <>
2023-08-23 13:26:34 +0000

    bug

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -86,14 +86,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
-# DATABASE_URL = os.getenv('DATABASE_URL')
 # DATABASES = {
-#     'default': dj_database_url.config(),
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 # }
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
